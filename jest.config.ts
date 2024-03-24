dafeb1f449007b0f915d681581f83a0a85dcf407
Mike Burgess <mddburgess@users.noreply.github.com>
2024-03-24 12:50:12 -0700

    Report test results in pull request workflow.

--- jest.config.ts
+++ jest.config.ts
@@ -110,7 +110,10 @@ const config: Config = {
   // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
+  reporters: [
+    "default",
+    "jest-junit"
+  ],
 
   // Automatically reset mock state before every test
   // resetMocks: false,
